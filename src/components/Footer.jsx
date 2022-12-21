d7f2a5def1e5fc4d0bc13be0d6a1d83f767611b4
D3press3d <anthonyrosman20@gmail.com>
2022-12-20 22:02:30 -0400

    :tada: add msg to outdated site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,27 @@
-import useLanguages from "../context/LanguageContext";
+import useLanguages from '../context/LanguageContext';
 //https://github.com/D3press3dd/portfolio-react link para colocar en el href del footer despues de hacer el readme.md
 const Footer = () => {
   const { language } = useLanguages();
   return (
-    <footer id="footer">
-      <a
-        href="https://github.com/D3press3dd"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {language === "en"
-          ? `Designed & built by Anthony Rosman©`
-          : `Diseñado y maquetado por © Anthony Rosman `}
-      </a>
+    <footer id='footer'>
+      <div>
+        <a
+          href='https://github.com/D3press3dd'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {language === 'en'
+            ? `Designed & built by Anthony Rosman©`
+            : `Diseñado y maquetado por © Anthony Rosman `}
+        </a>
+      </div>
+      <div>
+        <h2>
+          {language === 'en'
+            ? 'Outdated site, new under construction'
+            : 'Sitio desactualizado, nuevo en construcción'}
+        </h2>
+      </div>
     </footer>
   );
 };
--- src/components/RightEmail.jsx
+++ src/components/RightEmail.jsx
@@ -1,15 +1,26 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
+import useLanguages from '../context/LanguageContext';
 
 const RightEmail = () => {
+  const { language } = useLanguages();
+
+  const handleClick = e => {
+    e.preventDefault();
+  };
+
   return (
     //los estilos de este componente se encuentran en LEFTBARICON
     <motion.div
-      className="email"
+      className='email'
       initial={{ opacity: 0, translateX: 100 }}
       animate={{ opacity: 1, translateX: 0 }}
-      transition={{ type: "spring", duration: 1, delay: 0.3 }}
+      transition={{ type: 'spring', duration: 1, delay: 0.3 }}
     >
-      <a href="mailto:anthonyrosman20@gmail.com">Anthonyrosman20@gmail.com</a>
+      <a href='#' onClick={handleClick}>
+        {language === 'en'
+          ? 'Outdated site, new under construction'
+          : 'Sitio desactualizado, nuevo en construcción'}
+      </a>
     </motion.div>
   );
 };
