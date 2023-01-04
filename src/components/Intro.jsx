2c34c53c59a01849eef7e184d82d7d83487b5d04
D3press3d <anthonyrosman20@gmail.com>
2023-01-04 15:04:36 -0400

    :fix: fix the easter egg to show only once

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,40 +1,31 @@
-import useLanguages from "../context/LanguageContext";
-import { data } from "../data";
-import Background from "../img/background.jpg";
-import Description from "./Description";
-import LanguageButton from "./LanguageButton";
-import LinkToHome from "./LinkToHome";
-import { motion } from "framer-motion";
+import useLanguages from '../context/LanguageContext';
+import { data } from '../data';
+import Background from '../img/background.jpg';
+import Description from './Description';
+import LanguageButton from './LanguageButton';
+import LinkToHome from './LinkToHome';
+import { motion } from 'framer-motion';
 
 const Intro = () => {
   const { language } = useLanguages();
-  console.log(
-    "%c👀👀¿Que haces aqui en las developer tools?👀👀",
-    "color: #e9322f; font-size: 20px; background: #f9f9f9; padding: 10px; border-radius: 5px; box-shadow: 0 0 10px #e9322f;"
-  );
-
-  console.log(
-    "%cSi quieres ver el codigo fuente, esta en mi repositorio, si lo usas de inspiracion dejame una estrella",
-    "color: #e9322f; font-size: 16px; background: #f9f9f9; padding: 10px; border-radius: 5px; box-shadow: 0 0 10px #e9322f;"
-  );
 
   return (
     <>
       <LanguageButton />
       <motion.div
-        className="intro"
+        className='intro'
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 3 }}
         exit={{ opacity: 0, transition: { duration: 1 } }}
       >
-        <img src={Background} alt="background image" />
-        <motion.div className="intro-text">
-          {language === "en"
+        <img src={Background} alt='background image' />
+        <motion.div className='intro-text'>
+          {language === 'en'
             ? data.saludoEn
             : data.saludoEs.map((letra, index) => (
                 <motion.p
-                  className="intro-text"
+                  className='intro-text'
                   key={letra + index}
                   initial={{ opacity: 0, translateY: 150 }}
                   animate={{ opacity: 1, translateY: 0 }}
@@ -44,12 +35,12 @@ const Intro = () => {
                 </motion.p>
               ))}
         </motion.div>
-        <motion.div className="intro-text">
-          {language === "en"
+        <motion.div className='intro-text'>
+          {language === 'en'
             ? data.saludoEnSecond
             : data.saludoEsSecond.map((letra, index) => (
                 <motion.p
-                  className="intro-text"
+                  className='intro-text'
                   key={letra + index}
                   initial={{ opacity: 0, translateY: 150 }}
                   animate={{ opacity: 1, translateY: 0 }}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 
-import "./styles/styles.scss";
+import './styles/styles.scss';
+
+console.log(
+  '%c👀👀¿Que haces aqui en las developer tools?👀👀',
+  'color: #e9322f; font-size: 20px; background: #f9f9f9; padding: 10px; border-radius: 5px; box-shadow: 0 0 10px #e9322f;'
+);
+
+console.log(
+  '%cSi quieres ver el codigo fuente, esta en mi repositorio, si lo usas de inspiracion dejame una estrella',
+  'color: #e9322f; font-size: 16px; background: #f9f9f9; padding: 10px; border-radius: 5px; box-shadow: 0 0 10px #e9322f;'
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +21,5 @@ ReactDOM.render(
       <App />
     </HashRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
